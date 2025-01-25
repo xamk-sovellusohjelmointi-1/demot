10fab2ed35165bf9a10596e41ad99ca39333cfa0
Henri Riissanen <henri.riissanen@xamk.fi>
2025-01-25 14:39:52 +0200

    Updated demo 1

--- demo01/src/App.tsx
+++ demo01/src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App : React.FC = () : React.ReactElement => {
+
+  const [tervehdys, setTervehdys] = useState<string>("");
+  const [nimi, setNimi] = useState<string>("");
+
+  function naytaTervehdys() {
+    setTervehdys(`Heippa, ${nimi}`);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Demo 1: React-perusteet</h1>
+      <h2>"Hello World!"</h2>
+      <input type="text" placeholder='Kirjoita nimesi...' onChange={(e) => { setNimi(e.target.value); console.log(nimi); }}/>
+      <button onClick={naytaTervehdys}>Sano hei</button>
+
+      {(Boolean(tervehdys))
+      ? <p className='tervehdys'>{tervehdys}</p>
+      : null
+      }
+      
     </>
   )
 }
